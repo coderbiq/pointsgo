0350943b2f0f0ac60369358340b830189c6ce647
毕强 <biqiang_cd@keruyun.com>
2019-01-15 17:27:39 +0800

    创建一个使用内存的积分账户资源库

---
+++ base/account_repository.go
@@ -0,0 +1,41 @@
+package base
+
+import (
+	"errors"
+
+	"github.com/coderbiq/dgo/model"
+)
+
+type inMemoryAccountRepo struct {
+	accounts map[int64]Account
+}
+
+// NewInMemoryAccountRepo 创建积分账户资源库
+func NewInMemoryAccountRepo() AccountRepository {
+	return &inMemoryAccountRepo{
+		accounts: map[int64]Account{},
+	}
+}
+
+func (repo *inMemoryAccountRepo) Save(account Account) error {
+	repo.accounts[account.ID().Int64()] = account
+	return nil
+}
+
+func (repo inMemoryAccountRepo) Get(accountID model.LongID) (Account, error) {
+	account, has := repo.accounts[accountID.Int64()]
+	if has {
+		return nil, errors.New("account not found")
+	}
+	return account, nil
+}
+
+func (repo inMemoryAccountRepo) FindByOwner(ownerID model.LongID) ([]Account, error) {
+	accounts := []Account{}
+	for _, account := range repo.accounts {
+		if account.OwnerID().Equal(ownerID) {
+			accounts = append(accounts, account)
+		}
+	}
+	return accounts, nil
+}
