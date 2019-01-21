6fea6771b28c86b956bdde6405e916872bc56c2a
毕强 <biqiang_cd@keruyun.com>
2019-01-21 18:03:48 +0800

    创建新的账户查询方法

--- base/internal/infra/account_finder.go
+++ base/internal/infra/account_finder.go
@@ -1,6 +1,8 @@
 package infra
 
 import (
+	"errors"
+	"regexp"
 	"strconv"
 
 	"github.com/coderbiq/pointsgo/base/internal/model"
@@ -18,4 +20,35 @@ func (finder accountFinder) Detail(accountID int64) (model.AccountReader, error)
 	}
 
 	return model.AccountReader{}, nil
+}
+
+func (finder accountFinder) ByID(accountID int64, fields []string) (map[string]interface{}, error) {
+	datas := make(map[string]interface{})
+	if adatas, has := db.get(accountKey(strconv.FormatInt(accountID, 10))); has {
+		po := adatas.(accountPO)
+		logFields := []string{}
+		for _, field := range fields {
+			if matched, err := regexp.MatchString("logs.*", field); err == nil && matched {
+				logFields = append(logFields, field[5:])
+			} else {
+				if v, has := po.datas[field]; has {
+					datas[field] = v
+				}
+			}
+		}
+		if len(logFields) > 0 {
+			logs := []map[string]interface{}{}
+			for _, log := range po.logs {
+				l := make(map[string]interface{})
+				for _, field := range logFields {
+					l[field] = log[field]
+				}
+				logs = append(logs, l)
+			}
+			datas["logs"] = logs
+		}
+	} else {
+		return datas, errors.New("没有找到指定账户")
+	}
+	return datas, nil
 }
